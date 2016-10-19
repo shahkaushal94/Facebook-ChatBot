4fea0d8caa13ac216950b45a6e6c284225e5ed87
shahkaushal94 <shahkaushal94@gmail.com>
2016-10-19 11:30:17 -0700

    added callback

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-			if (!whatsmylunch(event.sender.id, event.message.text)) {
+			if (!TarlaDalal(event.sender.id, event.message.text)) {
         sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
         }
