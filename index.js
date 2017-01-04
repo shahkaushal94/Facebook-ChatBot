a605bbff58c292d6086e5f9bf78be0cfc5ac00fd
shahkaushal94 <shahkaushal94@gmail.com>
2017-01-04 11:13:10 -0800

    First commit

--- index.js
+++ index.js
@@ -57,54 +57,10 @@ function sendMessage(recipientId, message) {
 };
 
 
-// send rich message with kitten
-function kittenMessage(recipientId, text) {
-    
-    text = text || "";
-    var values = text.split(' ');
-    
-    if (values.length === 3 && values[0] === 'kitten') {
-        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-            
-            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-            
-            message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Kitten",
-                            "subtitle": "Cute kitten picture",
-                            "image_url": imageUrl ,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": imageUrl,
-                                "title": "Show kitten"
-                                }, {
-                                "type": "postback",
-                                "title": "I like this",
-                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                            }]
-                        }]
-                    }
-                }
-            };
-    
-            sendMessage(recipientId, message);
-            
-            return true;
-        }
-    }
-    
-    return false;
-    
-};
 
 
 
-
-// send rich message with kitten
+// send rich message with recommendation
 function TarlaDalal(recipientId, text) {
     
     text = text || "";
@@ -142,7 +98,7 @@ function TarlaDalal(recipientId, text) {
     
 };
 
-// send rich message with kitten
+// send rich message for "Something else"
 function SomethingElse(recipientId, text) {
     
     text = text || "";
@@ -185,7 +141,7 @@ function SomethingElse(recipientId, text) {
 
 
 
-// send rich message with kitten
+// send rich message with Ingredients
 function whatsmylunch(recipientId, text) {
     
     text = text || "";
